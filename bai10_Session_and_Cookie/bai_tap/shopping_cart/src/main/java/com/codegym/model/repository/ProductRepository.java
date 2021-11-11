cc571d850ab5e88ca8f8dbee6613bda4e0e7e3a6
namanhgithub98 <doanhnguyen260898>
2021-11-11 22:47:01 +0700

    up

---
+++ bai10_Session_and_Cookie/bai_tap/shopping_cart/src/main/java/com/codegym/model/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.codegym.model.repository;
+
+import com.codegym.model.entity.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+}
